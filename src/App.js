c35dce614a54e5bf157fd80869fde349987471c0
Stefan Bobrowski <stefanbobrowski1@gmail.com>
2021-04-07 22:58:17 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Album from './pages/Album/Album.jsx';
+import About from './pages/About/About.jsx';
+import Contact from './pages/Contact/Contact.jsx';
+import PageNotFound from './pages/PageNotFound/PageNotFound.jsx';
+import Header from './components/Header/Header.jsx';
+
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <Album albumName={'I'} />
+        </Route>
+        <Route exact path="/I">
+          <Album albumName={'I'} />
+        </Route>
+        <Route exact path="/II">
+          <Album albumName={'II'} />
+        </Route>
+        <Route exact path="/III">
+          <Album albumName={'III'} />
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route path="/contact">
+          <Contact />
+        </Route>
+
+        <Route component={PageNotFound} />
+      </Switch>
     </div>
   );
 }
